deb792e6e69f2d358d2bd3308eedcab2a6f70c19
lukebrobbs <lukebrobbin@lukes-mbp.home>
2018-05-25 23:11:15 +0100

    complete table builder functions

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,6 +2,92 @@ import React from "react";
 import "./Table.css";
 import tableBuilder from "../../utils/tableBuilder";
 
+// const testData = {
+//   name: "Group A",
+//   winner: null,
+//   runnerup: null,
+//   matches: [
+//     {
+//       name: 1,
+//       type: "group",
+//       home_team: 1,
+//       away_team: 2,
+//       home_result: 3,
+//       away_result: 2,
+//       date: "2018-06-14T18:00:00+03:00",
+//       stadium: 1,
+//       channels: [4, 6],
+//       finished: true,
+//       matchday: 1
+//     },
+//     {
+//       name: 2,
+//       type: "group",
+//       home_team: 3,
+//       away_team: 4,
+//       home_result: 1,
+//       away_result: 0,
+//       date: "2018-06-15T17:00:00+05:00",
+//       stadium: 12,
+//       channels: [3, 6],
+//       finished: true,
+//       matchday: 1
+//     },
+//     {
+//       name: 17,
+//       type: "group",
+//       home_team: 1,
+//       away_team: 3,
+//       home_result: 3,
+//       away_result: 10,
+//       date: "2018-06-19T21:00:00+03:00",
+//       stadium: 3,
+//       channels: [3, 6],
+//       finished: true,
+//       matchday: 2
+//     },
+//     {
+//       name: 18,
+//       type: "group",
+//       home_team: 4,
+//       away_team: 2,
+//       home_result: null,
+//       away_result: null,
+//       date: "2018-06-20T18:00:00+03:00",
+//       stadium: 10,
+//       channels: [3, 6],
+//       finished: false,
+//       matchday: 2
+//     },
+//     {
+//       name: 33,
+//       type: "group",
+//       home_team: 4,
+//       away_team: 1,
+//       home_result: 3,
+//       away_result: 1,
+//       date: "2018-06-25T18:00:00+04:00",
+//       stadium: 7,
+//       channels: [4, 6],
+//       finished: true,
+//       matchday: 3
+//     },
+//     {
+//       name: 34,
+//       type: "group",
+//       home_team: 2,
+//       away_team: 3,
+//       home_result: null,
+//       away_result: null,
+//       date: "2018-06-25T17:00:00+03:00",
+//       stadium: 8,
+//       channels: [5, 6],
+//       finished: false,
+//       matchday: 3
+//     }
+//   ]
+// };
+
 const Table = ({ teams, group }) => {
   return (
     <table className="table">
@@ -19,50 +105,21 @@ const Table = ({ teams, group }) => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">{`${teams[0].emojiString} ${teams[0].name}`}</th>
-          <td>{tableBuilder.getGamesPlayed(teams[0], group)}</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-        </tr>
-        <tr>
-          <th scope="row">{`${teams[1].emojiString} ${teams[1].name}`}</th>
-          <td>{tableBuilder.getGamesPlayed(teams[1], group)}</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-        </tr>
-        <tr>
-          <th scope="row">{`${teams[2].emojiString} ${teams[2].name}`}</th>
-          <td>{tableBuilder.getGamesPlayed(teams[2], group)}</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-        </tr>
-        <tr>
-          <th scope="row">{`${teams[3].emojiString} ${teams[3].name}`}</th>
-          <td>{tableBuilder.getGamesPlayed(teams[3], group)}</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-          <td>0</td>
-        </tr>
+        {teams.map((team, i) => {
+          return (
+            <tr key={i}>
+              <th scope="row">{`${team.emojiString} ${team.name}`}</th>
+              <td>{tableBuilder.getGamesPlayed(team.id, group.matches)}</td>
+              <td>{tableBuilder.getGamesWon(team.id, group.matches)}</td>
+              <td>{tableBuilder.getGamesDrawn(team.id, group.matches)}</td>
+              <td>{tableBuilder.getGamesLost(team.id, group.matches)}</td>
+              <td>{tableBuilder.getGoalsFor(team.id, group.matches)}</td>
+              <td>{tableBuilder.getGoalsAgainst(team.id, group.matches)}</td>
+              <td>{tableBuilder.getGoalDifference(team.id, group.matches)}</td>
+              <td>{tableBuilder.getPoints(team.id, group.matches)}</td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
--- src/utils/tableBuilder.js
+++ src/utils/tableBuilder.js
@@ -10,24 +10,96 @@ const tableBuilder = {
       return acc;
     }, []);
   },
-  getGamesPlayed: (team, group) => {
-    const { id } = team;
-    const { matches } = group;
+  getGamesPlayed: (teamId, matches) => {
     return matches.reduce((acc, curr) => {
       acc +=
-        (curr.home_team === id || curr.away_team === id) && curr.finished
+        (curr.home_team === teamId || curr.away_team === teamId) &&
+        curr.finished
           ? 1
           : 0;
       return acc;
     }, 0);
   },
-  getGamesWon: (team, group) => {},
-  getGamesDrawn: (team, group) => {},
-  getGamesLost: (team, group) => {},
-  getGoalsFor: (team, group) => {},
-  getGoalsAgainst: (team, group) => {},
-  getGoalDifference: (team, group) => {},
-  getPoints: (team, group) => {}
+  getGamesWon: (teamId, matches) => {
+    return matches.reduce((acc, match) => {
+      if (!match.finished) return acc;
+      else {
+        acc +=
+          match.home_team === teamId && match.home_result > match.away_result
+            ? 1
+            : 0;
+        acc +=
+          match.away_team === teamId && match.home_result < match.away_result
+            ? 1
+            : 0;
+        return acc;
+      }
+    }, 0);
+  },
+  getGamesDrawn: (teamId, matches) => {
+    return matches.reduce((acc, match) => {
+      if (!match.finished) return acc;
+      else {
+        acc +=
+          match.home_team === teamId && match.home_result === match.away_result
+            ? 1
+            : 0;
+        acc +=
+          match.away_team === teamId && match.home_result === match.away_result
+            ? 1
+            : 0;
+        return acc;
+      }
+    }, 0);
+  },
+  getGamesLost: (teamId, matches) => {
+    return matches.reduce((acc, match) => {
+      if (!match.finished) return acc;
+      else {
+        acc +=
+          match.home_team === teamId && match.home_result < match.away_result
+            ? 1
+            : 0;
+        acc +=
+          match.away_team === teamId && match.home_result > match.away_result
+            ? 1
+            : 0;
+        return acc;
+      }
+    }, 0);
+  },
+  getGoalsFor: (teamId, matches) => {
+    return matches.reduce((acc, match) => {
+      if (!match.finished) return acc;
+      else {
+        acc += match.home_team === teamId ? match.home_result : 0;
+        acc += match.away_team === teamId ? match.away_result : 0;
+        return acc;
+      }
+    }, 0);
+  },
+  getGoalsAgainst: (teamId, matches) => {
+    return matches.reduce((acc, match) => {
+      if (!match.finished) return acc;
+      else {
+        acc += match.home_team === teamId ? match.away_result : 0;
+        acc += match.away_team === teamId ? match.home_result : 0;
+        return acc;
+      }
+    }, 0);
+  },
+  getGoalDifference: (teamId, matches) => {
+    return (
+      tableBuilder.getGoalsFor(teamId, matches) -
+      tableBuilder.getGoalsAgainst(teamId, matches)
+    );
+  },
+  getPoints: (teamId, matches) => {
+    return (
+      tableBuilder.getGamesWon(teamId, matches) * 3 +
+      tableBuilder.getGamesDrawn(teamId, matches)
+    );
+  }
 };
 
 export default tableBuilder;
