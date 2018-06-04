6a83597a4081c8793be3d0998f19d4106e3b7511
lukebrobbs <lukebrobbin@Lukes-MacBook-Pro.local>
2018-06-04 10:48:13 +0100

    add sort function to table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -90,6 +90,21 @@ import { Table } from "react-bootstrap";
 // };
 
 const LeagueTable = ({ teams, group }) => {
+  const entries = teams.map(team => {
+    return {
+      name: team.name,
+      flag: team.emojiString,
+      played: tableBuilder.getGamesPlayed(team.id, group.matches),
+      won: tableBuilder.getGamesWon(team.id, group.matches),
+      drawn: tableBuilder.getGamesDrawn(team.id, group.matches),
+      lost: tableBuilder.getGamesLost(team.id, group.matches),
+      goalsFor: tableBuilder.getGoalsFor(team.id, group.matches),
+      goalsAgainst: tableBuilder.getGoalsAgainst(team.id, group.matches),
+      goalDifference: tableBuilder.getGoalDifference(team.id, group.matches),
+      points: tableBuilder.getPoints(team.id, group.matches)
+    };
+  });
+  const sortedEntries = tableBuilder.sortTable(entries);
   return (
     <Table hover>
       <thead>
@@ -110,18 +125,18 @@ const LeagueTable = ({ teams, group }) => {
         </tr>
       </thead>
       <tbody>
-        {teams.map((team, i) => {
+        {sortedEntries.map((team, i) => {
           return (
             <tr key={i}>
-              <th scope="row">{`${team.emojiString} ${team.name}`}</th>
-              <td>{tableBuilder.getGamesPlayed(team.id, group.matches)}</td>
-              <td>{tableBuilder.getGamesWon(team.id, group.matches)}</td>
-              <td>{tableBuilder.getGamesDrawn(team.id, group.matches)}</td>
-              <td>{tableBuilder.getGamesLost(team.id, group.matches)}</td>
-              <td>{tableBuilder.getGoalsFor(team.id, group.matches)}</td>
-              <td>{tableBuilder.getGoalsAgainst(team.id, group.matches)}</td>
-              <td>{tableBuilder.getGoalDifference(team.id, group.matches)}</td>
-              <td>{tableBuilder.getPoints(team.id, group.matches)}</td>
+              <th scope="row">{`${team.flag} ${team.name}`}</th>
+              <td>{team.played}</td>
+              <td>{team.won}</td>
+              <td>{team.drawn}</td>
+              <td>{team.lost}</td>
+              <td>{team.goalsFor}</td>
+              <td>{team.goalsAgainst}</td>
+              <td>{team.goalDifference}</td>
+              <td>{team.points}</td>
             </tr>
           );
         })}
--- src/utils/tableBuilder.js
+++ src/utils/tableBuilder.js
@@ -99,6 +99,48 @@ const tableBuilder = {
       tableBuilder.getGamesWon(teamId, matches) * 3 +
       tableBuilder.getGamesDrawn(teamId, matches)
     );
+  },
+  sortTable: table => {
+    const nameSort = table.sort((a, b) => {
+      return a.name === b.name ? 0 : a.name < b.name ? -1 : 1;
+    });
+    const goalsAgainstSort = nameSort.sort((a, b) => {
+      if (a.getGoalsAgainst < b.getGoalsAgainst) {
+        return 1;
+      }
+      if (a.getGoalsAgainst > b.getGoalsAgainst) {
+        return -1;
+      }
+      return 0;
+    });
+    const goalsForSort = goalsAgainstSort.sort((a, b) => {
+      if (a.getGoalsFor > b.getGoalsFor) {
+        return 1;
+      }
+      if (a.getGoalsFor < b.getGoalsFor) {
+        return -1;
+      }
+      return 0;
+    });
+    const goalDifferenceSort = goalsForSort.sort((a, b) => {
+      if (a.goalDifference > b.goalDifference) {
+        return 1;
+      }
+      if (a.goalDifference < b.goalDifference) {
+        return -1;
+      }
+      return 0;
+    });
+
+    return goalDifferenceSort.sort((a, b) => {
+      if (a.points > b.points) {
+        return 1;
+      }
+      if (a.points < b.points) {
+        return -1;
+      }
+      return 0;
+    });
   }
 };
 
